d25accb3134d11214b304c74ceb010144ee422e5
yingan3 <yingnan3@staff.sina.com.cn>
2026-07-29 15:48:43 +0800

    Add MCP Task Trace to tool directory

--- ops/worker-proxy.js
+++ ops/worker-proxy.js
@@ -1,5 +1,5 @@
 const UPSTREAM = "https://guagua-tools.pages.dev";
-const BUILD_REVISION = "20260729-mcpapps";
+const BUILD_REVISION = "20260729-mcptasks";
 
 export default {
   async fetch(request) {
